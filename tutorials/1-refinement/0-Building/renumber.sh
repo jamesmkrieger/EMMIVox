19e472d76173979e5c47b9848b328751f09dad93
James Krieger <jamesmkrieger@gmail.com>
2025-08-22 18:21:15 +0100

    add dir to renumber.sh

--- tutorials/1-refinement/0-Building/renumber.sh
+++ tutorials/1-refinement/0-Building/renumber.sh
@@ -2,17 +2,19 @@
 gro=$1
 # pdb file
 pdb=$2
+# directory
+dir={$3:-$pwd}
 
 # rename old files
-mv $gro old_$gro
-mv $pdb old_$pdb
+cp ${dir}/$gro ${dir}/old_$gro
+cp ${dir}/$pdb ${dir}/old_$pdb
 
 # create a fake tpr file
-gmx_mpi grompp -f ../2-Equilibration/0-em-steep.mdp -c old_$gro -p topol.top -o fake.tpr
+gmx_mpi grompp -f ${dir}../2-Equilibration/0-em-steep.mdp -c ${dir}/old_$gro -p ${dir}/topol.top -o ${dir}/fake.tpr
 
 # convert old gro to new gro and pdb
-echo 0 | gmx_mpi trjconv -f old_$gro -o $gro -s fake.tpr
-echo 0 | gmx_mpi trjconv -f old_$gro -o $pdb -s fake.tpr
+echo 0 | gmx_mpi trjconv -f ${dir}/old_$gro -o ${dir}/$gro -s ${dir}/fake.tpr
+echo 0 | gmx_mpi trjconv -f ${dir}/old_$gro -o ${dir}/$pdb -s ${dir}/fake.tpr
 
 # clean
-rm fake.tpr
+rm ${dir}/fake.tpr
