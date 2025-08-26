2709305b7a23af3b16581e2e8ee8f8fb0e830d50
James Krieger <jamesmkrieger@gmail.com>
2025-08-26 18:02:08 +0100

    add directories and top to renumber.sh

--- tutorials/1-refinement/0-Building/renumber.sh
+++ tutorials/1-refinement/0-Building/renumber.sh
@@ -2,19 +2,28 @@
 gro=$1
 # pdb file
 pdb=$2
-# directory
-dir={$3:-$pwd}
+# directory containing mdp
+ref_dir=${3:-$(pwd)}
+ref_dir=$(realpath "$ref_dir")
+# output directory
+out_dir=${4:-$(pwd)}
+out_dir=$(realpath "$out_dir")
+# top file
+top=${5:-"$ref_dir/topol.top"}
+echo $top
 
 # rename old files
-cp ${dir}/$gro ${dir}/old_$gro
-cp ${dir}/$pdb ${dir}/old_$pdb
+copied_gro=${out_dir}/old_$(basename "$gro")
+copied_pdb=${out_dir}/old_$(basename "$pdb")
+cp $gro $copied_gro
+cp $pdb $copied_pdb
 
 # create a fake tpr file
-gmx_mpi grompp -f ${dir}../2-Equilibration/0-em-steep.mdp -c ${dir}/old_$gro -p ${dir}/topol.top -o ${dir}/fake.tpr
+gmx_mpi grompp -f ${ref_dir}/../2-Equilibration/0-em-steep.mdp -c $copied_gro -p $top -o ${out_dir}/fake.tpr
 
-# convert old gro to new gro and pdb
-echo 0 | gmx_mpi trjconv -f ${dir}/old_$gro -o ${dir}/$gro -s ${dir}/fake.tpr
-echo 0 | gmx_mpi trjconv -f ${dir}/old_$gro -o ${dir}/$pdb -s ${dir}/fake.tpr
+# # convert old gro to new gro and pdb
+echo 0 | gmx_mpi trjconv -f $copied_gro -o ${out_dir}/$(basename "$gro") -s ${out_dir}/fake.tpr
+echo 0 | gmx_mpi trjconv -f $copied_gro -o ${out_dir}/$(basename "$pdb") -s ${out_dir}/fake.tpr
 
-# clean
-rm ${dir}/fake.tpr
+# # clean
+rm ${out_dir}/fake.tpr
