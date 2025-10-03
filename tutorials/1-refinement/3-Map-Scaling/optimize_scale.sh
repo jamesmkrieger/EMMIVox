82414091dcce72eaf743b595c1f02a8be27dd9de
James Krieger <jamesmkrieger@gmail.com>
2025-10-03 15:05:18 +0100

    optimize scale input args

--- tutorials/1-refinement/3-Map-Scaling/optimize_scale.sh
+++ tutorials/1-refinement/3-Map-Scaling/optimize_scale.sh
@@ -1,12 +1,20 @@
 #!/bin/bash
+
+logfile=${1:"../1-Map-Preparation/log.preprocess"}
+gro=${2:"../2-Equilibration/em.gro"}
+ndx=${3:"../0-Building/index.ndx"}
+pdb=${4:"step3_input_xtc.pdb"}
+tpr=${5:"../2-Equilibration/em.tpr"}
+xtc=${6:"../../2-Equilibration/nvt_posres.xtc"}
+
 # number of CPU cores used by PLUMED 
 export PLUMED_NUM_THREADS=$1
 # extract NORM_DENSITY and RESOLUTION from `../1-Map-Preparation/log.preprocess` 
-n=`grep NORM_DENSITY ../1-Map-Preparation/log.preprocess | awk '{print $NF}'`
-r=`grep Resolution ../1-Map-Preparation/log.preprocess | awk '{print $NF/10.0}'`
+n=`grep NORM_DENSITY $logfile | awk '{print $NF}'`
+r=`grep Resolution $logfile | awk '{print $NF/10.0}'`
 
 # create a PDB file with only the XTC atoms
-echo System-XTC | gmx_mpi trjconv -f ../2-Equilibration/em.gro -n ../0-Building/index.ndx -o step3_input_xtc.pdb -pbc nojump -s ../2-Equilibration/em.tpr
+echo System-XTC | gmx_mpi trjconv -f $gro -n $ndx -o $pdb -pbc nojump -s $tpr
 
 # loop over scale values
 for d in $(seq 0.7 0.05 1.3)
@@ -16,7 +24,7 @@ do
         # create plumed input file for postprocessing
         sed -e "s/NORM_DENSITY_/$n/g" ../plumed_EMMI_template_BFACT.dat | sed -e "s/RESOLUTION_/$r/g" | sed -e "s/SCALE_/$d/g" > plumed.dat
         # run PLUMED driver to calculate EMMIVOX score
-        plumed driver --plumed plumed.dat --mf_xtc ../../2-Equilibration/nvt_posres.xtc > log.plumed
+        plumed driver --plumed plumed.dat --mf_xtc $xtc > log.plumed
         # go back to root 	
 	cd ../
 done
