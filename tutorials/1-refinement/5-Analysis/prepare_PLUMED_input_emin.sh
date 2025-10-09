ea72101d1c749390626612a6ee48033200b3cd97
James Krieger <jamesmkrieger@gmail.com>
2025-10-09 16:59:54 +0100

    input args for analysis

--- tutorials/1-refinement/5-Analysis/prepare_PLUMED_input_emin.sh
+++ tutorials/1-refinement/5-Analysis/prepare_PLUMED_input_emin.sh
@@ -1,13 +1,31 @@
 #!/bin/bash        
-DDIR="../4-Production"
+DDIR=${1:-"../4-Production"}
 # extract NORM_DENSITY and RESOLUTION from `../1-Map-Preparation/log.preprocess` 
 # and BEST_SCALE from ../3-Map-Scaling/BEST_SCALE
-n=`grep NORM_DENSITY ../1-Map-Preparation/log.preprocess | awk '{print $NF}'`
-r=`grep Resolution ../1-Map-Preparation/log.preprocess | awk '{print $NF/10.0}'`
-s=`grep BEST_SCALE ../3-Map-Scaling/BEST_SCALE | awk '{print $NF}'`
+export logfile=${2:-"../1-Map-Preparation/log.preprocess"}
+export bestscalefile=${3:-"../3-Map-Scaling/BEST_SCALE"}
+
+export ndx=${4:-"../0-Building/index.ndx"}
+ndx=$(realpath --relative-to=. "$ndx")
+
+export pdb=${5:-"../3-Map-Scaling/step3_input_xtc.pdb"}
+pdb=$(realpath --relative-to=. "$pdb")
+
+export datafile=${6:-"../../1-Map-Preparation/emd_plumed_aligned.dat"}
+datafile=$(realpath --relative-to=. "$datafile")
+
+# extract NORM_DENSITY and RESOLUTION from `../1-Map-Preparation/log.preprocess` 
+# and BEST_SCALE from ../3-Map-Scaling/BEST_SCALE
+n=`grep NORM_DENSITY $logfile | awk '{print $NF}'`
+r=`grep Resolution $logfile | awk '{print $NF/10.0}'`
+s=`grep BEST_SCALE $bestscalefile | awk '{print $NF}'`
 
 # create plumed input file for production 
-sed -e "s/NORM_DENSITY_/$n/g" plumed_EMMI_emin_template.dat | sed -e "s/RESOLUTION_/$r/g" | sed -e "s/SCALE_/$s/g" > plumed_EMMI_emin.dat
+sed -e "s/NORM_DENSITY_/$n/g" plumed_EMMI_emin_template.dat \
+    -e "s/RESOLUTION_/$r/g" -e "s/SCALE_/$s/g" \
+    -e "s|../3-Map-Scaling/step3_input_xtc.pdb|$pdb|g" \
+    -e "s|../0-Building/index.ndx|$ndx|g" \
+    -e "s|../1-Map-Preparation/emd_plumed_aligned.dat|$datafile|g" > plumed_EMMI_emin.dat
 
 # Extract lowest energy frame from the single structure refinement (4-Production)
 # get time (ps) of the frame with best score
