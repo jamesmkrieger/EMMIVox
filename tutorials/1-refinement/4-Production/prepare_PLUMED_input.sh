087b3d2cd241f61fdf59ae19898737caa6e3c69a
James Krieger <jamesmkrieger@gmail.com>
2025-10-09 09:33:27 +0100

    adjust refine prod prep for diff directory

--- tutorials/1-refinement/4-Production/prepare_PLUMED_input.sh
+++ tutorials/1-refinement/4-Production/prepare_PLUMED_input.sh
@@ -1,9 +1,27 @@
 #!/bin/bash
+export logfile=${1:-"../1-Map-Preparation/log.preprocess"}
+export bestscalefile=${2:-"../3-Map-Scaling/BEST_SCALE"}
+
+export ndx=${3:-"../0-Building/index.ndx"}
+ndx=$(realpath --relative-to=. "$ndx")
+
+export pdb=${4:-"../3-Map-Scaling/step3_input_xtc.pdb"}
+pdb=$(realpath --relative-to=. "$pdb")
+
+export datafile=${5:-"../../1-Map-Preparation/emd_plumed_aligned.dat"}
+datafile=$(realpath --relative-to=. "$datafile")
+
 # extract NORM_DENSITY and RESOLUTION from `../1-Map-Preparation/log.preprocess` 
 # and BEST_SCALE from ../3-Map-Scaling/BEST_SCALE
-n=`grep NORM_DENSITY ../1-Map-Preparation/log.preprocess | awk '{print $NF}'`
-r=`grep Resolution ../1-Map-Preparation/log.preprocess | awk '{print $NF/10.0}'`
-s=`grep BEST_SCALE ../3-Map-Scaling/BEST_SCALE | awk '{print $NF}'`
+n=`grep NORM_DENSITY $logfile | awk '{print $NF}'`
+r=`grep Resolution $logfile | awk '{print $NF/10.0}'`
+s=`grep BEST_SCALE $bestscalefile | awk '{print $NF}'`
+
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
 # create plumed input file for production 
-sed -e "s/NORM_DENSITY_/$n/g" plumed_EMMI_template.dat | sed -e "s/RESOLUTION_/$r/g" | sed -e "s/SCALE_/$s/g" > plumed_EMMI.dat
+sed -e "s/NORM_DENSITY_/$n/g" ${SCRIPT_DIR}/plumed_EMMI_template.dat \
+    -e "s/RESOLUTION_/$r/g" -e "s/SCALE_/$s/g" \
+    -e "s|../3-Map-Scaling/step3_input_xtc.pdb|$pdb|g" \
+    -e "s|../0-Building/index.ndx|$ndx|g" \
+    -e "s|../1-Map-Preparation/emd_plumed_aligned.dat|$datafile|g" > plumed_EMMI.dat
