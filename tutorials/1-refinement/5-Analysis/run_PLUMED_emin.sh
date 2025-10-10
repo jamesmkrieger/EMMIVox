ddf14f1522cb281f2fd4d486146da5f46c85e416
James Krieger <jamesmkrieger@gmail.com>
2025-10-10 11:18:24 +0100

    run emin fix args

--- tutorials/1-refinement/5-Analysis/run_PLUMED_emin.sh
+++ tutorials/1-refinement/5-Analysis/run_PLUMED_emin.sh
@@ -1,11 +1,11 @@
 # Run energy minimization
-export mdp=${1:-"../2-Equilibration/0-em-steep.mdp"}
+export mdp=${2:-"../2-Equilibration/0-em-steep.mdp"}
 mdp=$(realpath --relative-to=. "$mdp")
 
-export topol=${2:-"../0-Building/topol.top"}
+export topol=${3:-"../0-Building/topol.top"}
 topol=$(realpath --relative-to=. "$topol")
 
-export ndx=${3:-"../0-Building/index.ndx"}
+export ndx=${4:-"../0-Building/index.ndx"}
 ndx=$(realpath --relative-to=. "$ndx")
 
 gmx_mpi grompp -f $mdp -c conf_best.gro -p $topol -o emin.tpr
