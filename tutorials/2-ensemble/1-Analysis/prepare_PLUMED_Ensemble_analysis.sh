f2446f043393fbddc6fd3c7a95c856eeac6e0588
James Krieger <jamesmkrieger@gmail.com>
2025-10-10 11:47:26 +0100

    replace ndx and pdb in all fix pbc dat

--- tutorials/2-ensemble/1-Analysis/prepare_PLUMED_Ensemble_analysis.sh
+++ tutorials/2-ensemble/1-Analysis/prepare_PLUMED_Ensemble_analysis.sh
@@ -1,10 +1,20 @@
+export ndx=${1:-"./../1-refinement/0-Building/index.ndx"}
+ndx=$(realpath --relative-to=. "$ndx")
+
+export pdb=${2:-"../../1-refinement/3-Map-Scaling/step3_input_xtc.pdb"}
+pdb=$(realpath --relative-to=. "$pdb")
+
 # 1) concatenate all the trajectories from multiple replicas -> traj-all.xtc
 gmx_mpi trjcat -f ../0-Production/rep-*/traj*.xtc -cat -o traj-all.xtc
 
-SCRIPT_DIR="$(dirname "${BASH_SOURCE[0]}")"
 # 2) fix PBCs with PLUMED and select only map atoms -> traj-all-PBC.xtc
-plumed driver --plumed ${SCRIPT_DIR}/plumed_fix_pbc.dat --mf_xtc traj-all.xtc
+SCRIPT_DIR="$(dirname "${BASH_SOURCE[0]}")"
+sed ${SCRIPT_DIR}/plumed_fix_pbc.dat \
+    -e "s|../../1-refinement/3-Map-Scaling/step3_input_xtc.pdb|$pdb|g" \
+    -e "s|./../1-refinement/0-Building/index.ndx|$ndx|g" > plumed_fix_pbc.dat
+
+plumed driver --plumed plumed_fix_pbc.dat --mf_xtc traj-all.xtc
 
 # 3) extract from step3_input_xtc.pdb only the atoms used to generate the cryo-em map. 
-echo System-MAP-H | gmx_mpi trjconv -f ../../1-refinement/3-Map-Scaling/step3_input_xtc.pdb -s ../0-Production/rep-00/production.tpr -n ../../1-refinement/0-Building/index.ndx -o conf_map.pdb
+echo System-MAP-H | gmx_mpi trjconv -f $pdb -s ../0-Production/rep-00/production.tpr -n $ndx -o conf_map.pdb
 
--- tutorials/4-ensemble-wat/1-Analysis/prepare_PLUMED_Ensemble_analysis.sh
+++ tutorials/4-ensemble-wat/1-Analysis/prepare_PLUMED_Ensemble_analysis.sh
@@ -1,9 +1,19 @@
+export ndx=${1:-"../../3-refinement-wat/0-Building/index.ndx"}
+ndx=$(realpath --relative-to=. "$ndx")
+
+export pdb=${2:-"../../3-refinement-wat/3-Map-Scaling/step3_input_xtc.pdb"}
+pdb=$(realpath --relative-to=. "$pdb")
+
 # 1) concatenate all the trajectories from multiple replicas -> traj-all.xtc
 gmx_mpi trjcat -f ../0-Production/rep-*/traj*.xtc -cat -o traj-all.xtc
 
 # 2) fix PBCs with PLUMED and select only map atoms -> traj-all-PBC.xtc
 SCRIPT_DIR="$(dirname "${BASH_SOURCE[0]}")"
-plumed driver --plumed ${SCRIPT_DIR}/plumed_fix_pbc.dat --mf_xtc traj-all.xtc
+sed ${SCRIPT_DIR}/plumed_fix_pbc.dat \
+    -e "s|../../3-refinement-wat/3-Map-Scaling/step3_input_xtc.pdb|$pdb|g" \
+    -e "s|../../3-refinement-wat/0-Building/index.ndx|$ndx|g" > plumed_fix_pbc.dat
+
+plumed driver --plumed plumed_fix_pbc.dat --mf_xtc traj-all.xtc
 
 # 3) extract from step3_input_xtc.pdb only the atoms used to generate the cryo-em map. 
-echo System-MAP-H | gmx_mpi trjconv -f ../../3-refinement-wat/3-Map-Scaling/step3_input_xtc.pdb -s ../0-Production/rep-00/production.tpr -n ../../3-refinement-wat/0-Building/index.ndx -o conf_map.pdb
+echo System-MAP-H | gmx_mpi trjconv -f $pdb -s ../0-Production/rep-00/production.tpr -n $ndx -o conf_map.pdb
