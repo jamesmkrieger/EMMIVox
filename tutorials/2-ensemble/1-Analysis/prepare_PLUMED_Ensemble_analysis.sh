75bedfefd9435126f03b959a6d36c63668e5bcf1
James Krieger <jamesmkrieger@gmail.com>
2025-10-13 15:15:26 +0100

    fix missing dot

--- tutorials/2-ensemble/1-Analysis/prepare_PLUMED_Ensemble_analysis.sh
+++ tutorials/2-ensemble/1-Analysis/prepare_PLUMED_Ensemble_analysis.sh
@@ -14,7 +14,7 @@ gmx_mpi trjcat -f ${DDIR}/rep-*/traj*.xtc -cat -o traj-all.xtc
 SCRIPT_DIR="$(dirname "${BASH_SOURCE[0]}")"
 sed ${SCRIPT_DIR}/plumed_fix_pbc.dat \
     -e "s|../../1-refinement/3-Map-Scaling/step3_input_xtc.pdb|$pdb|g" \
-    -e "s|./../1-refinement/0-Building/index.ndx|$ndx|g" > plumed_fix_pbc.dat
+    -e "s|../../1-refinement/0-Building/index.ndx|$ndx|g" > plumed_fix_pbc.dat
 
 plumed driver --plumed plumed_fix_pbc.dat --mf_xtc traj-all.xtc
 
