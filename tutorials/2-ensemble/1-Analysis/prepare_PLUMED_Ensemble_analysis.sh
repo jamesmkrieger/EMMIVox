14ccc07686eba6d10e44e4749fa356837dfc52f3
James Krieger <jamesmkrieger@gmail.com>
2025-10-13 15:09:43 +0100

    add production data dir arg to prep ens analysis

--- tutorials/2-ensemble/1-Analysis/prepare_PLUMED_Ensemble_analysis.sh
+++ tutorials/2-ensemble/1-Analysis/prepare_PLUMED_Ensemble_analysis.sh
@@ -4,8 +4,11 @@ ndx=$(realpath --relative-to=. "$ndx")
 export pdb=${2:-"../../1-refinement/3-Map-Scaling/step3_input_xtc.pdb"}
 pdb=$(realpath --relative-to=. "$pdb")
 
+export DDIR=${3:-"../0-Production"}
+DDIR=$(realpath --relative-to=. "$DDIR")
+
 # 1) concatenate all the trajectories from multiple replicas -> traj-all.xtc
-gmx_mpi trjcat -f ../0-Production/rep-*/traj*.xtc -cat -o traj-all.xtc
+gmx_mpi trjcat -f ${DDIR}/rep-*/traj*.xtc -cat -o traj-all.xtc
 
 # 2) fix PBCs with PLUMED and select only map atoms -> traj-all-PBC.xtc
 SCRIPT_DIR="$(dirname "${BASH_SOURCE[0]}")"
@@ -16,5 +19,5 @@ sed ${SCRIPT_DIR}/plumed_fix_pbc.dat \
 plumed driver --plumed plumed_fix_pbc.dat --mf_xtc traj-all.xtc
 
 # 3) extract from pdb only the atoms used to generate the cryo-em map. 
-echo System-MAP-H | gmx_mpi trjconv -f $pdb -s ../0-Production/rep-00/production.tpr -n $ndx -o conf_map.pdb
+echo System-MAP-H | gmx_mpi trjconv -f $pdb -s ${DDIR}/rep-00/production.tpr -n $ndx -o conf_map.pdb
 
